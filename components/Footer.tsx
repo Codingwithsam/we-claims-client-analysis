e9f32ab667e9c734dc380b542e2c06ae204c9e42
samuelbalogun <sambal523@gmail.com>
2025-10-29 19:01:19 -0400

    Implement PostHog

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                Contact Us +1(877)-947-2090
+            </div>
+        </footer>
+    )
+}
+export default Footer
